1cf58a0e934a06fd1a37b29e5af20c503bc8aed5
MarkJoaquin <markjoaquin2@gmail.com>
2025-01-27 17:15:14 -0800

    still ...

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,6 @@
 import { app } from "./app";
 import { ENV, PORT } from "./env";
-import { PrismaClient } from "@prisma/client/extension";
 
-const prisma = new PrismaClient();
 
 app.listen(PORT, () => {
   (`[server]: listening at http://localhost:${PORT} in ${ENV} mode`);
