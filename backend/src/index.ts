2ef13b362f225089240b259b654a3f79f7006bb1
MarkJoaquin <markjoaquin2@gmail.com>
2025-05-22 23:03:13 -0700

    feat: initialize backend server with Express and Vercel deployment config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,5 +2,5 @@ import { app } from "./app";
 import { ENV, PORT } from "./env";
 
 app.listen(PORT, () => {
-  (`[server]: listening at http://localhost:${PORT} in ${ENV} mode`);
+  console.log(`[server]: listening at http://localhost:${PORT} in ${ENV} mode`);
 });
